4ec76c6fd1d0bbd8a8f6648b262523ff2a63a37d
MiloszRoman <mioszroman.kontakt@gmail.com>
2025-05-01 19:12:28 +0200

    Add comments and and get article titles and descriptions

--- main.py
+++ main.py
@@ -5,8 +5,14 @@ api_key = "4224d33c07df4f80bc495528bff66e70"
 url = "https://newsapi.org/v2/everything?q=apple&from=2025-04-"\
       "30&to=2025-04-30&sortBy=popularity&apiKey=4224d33c07df4f80bc495528bff66e70"
 
+#Make a request
 request = requests.get(url)
 
-content = request.text
+#Get a dictionary with data
+content = request.json()
 
-print(content)
+#Access the article titles and description
+for article in content['articles']:
+      print(article['title'])
+      print(article['description'])
+      print("Something else \n\n\n\n\n")
