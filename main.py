ab375840b8b59bd198354c410a1dd785abc3e2f3
MiloszRoman <mioszroman.kontakt@gmail.com>
2025-05-01 20:01:59 +0200

    Add a titles and descriptions to a separate lists and one dictionary

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests
-
+from send_emails import send_email
 
 api_key = "4224d33c07df4f80bc495528bff66e70"
 url = "https://newsapi.org/v2/everything?q=apple&from=2025-04-"\
@@ -11,8 +11,29 @@ request = requests.get(url)
 #Get a dictionary with data
 content = request.json()
 
+titles = []
+descriptions = []
+
+dict = {}
+
+
 #Access the article titles and description
 for article in content['articles']:
-      print(article['title'])
-      print(article['description'])
-      print("Something else \n\n\n\n\n")
+      titles.append(article['title'])
+      descriptions.append(article['description'])
+      dict.update({article['title']: article['description']})
+
+
+raw_message = f"""
+Subject: 5 news from apis
+
+Title: {titles[0]}
+
+Description: {descriptions[0]}
+
+"""
+"""
+decision = input("Type if you want to send email: (Y/N) ")
+if decision == 'Y':
+      send_email(raw_message)
+"""
---
+++ send_emails.py
@@ -0,0 +1,18 @@
+import smtplib, ssl
+import os
+
+def send_email(message):
+    """Function for sending emails"""
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "mioszroman@gmail.com"
+    password = os.getenv("PASSWORD")
+    receiver = "mioszroman.kontakt@gmail.com"
+
+    context = ssl.create_default_context()
+    my_message = message
+
+    with smtplib.SMTP_SSL(host=host, port=port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, my_message)
